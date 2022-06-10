ada279112217c14a1a77441bef64610a9a5a53f7
Rob Thomure <robthomure@outlook.com>
2022-06-09 20:24:18 -0500

    Stage 1: Determine how difficult the text is judging by the average count of words in the sentence.

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input.length() <= 100) {
+            System.out.println("EASY");
+        } else {
+            System.out.println("HARD");
+        }
+    }
+}
